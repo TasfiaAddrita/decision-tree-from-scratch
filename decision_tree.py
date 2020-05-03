5f030b762f568fc73feec11e1917857c4d8bd91c
TasfiaAddrita <tasfia.addrita@gmail.com>
2020-05-03 13:02:16 -0700

    [add] starter file

---
+++ decision_tree.py
@@ -0,0 +1,18 @@
+class DecisionTreeClassifier:
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def visualize(self):
+        pass
+
+    def evaluate(self):
+        pass
+
+    def compare_with_sklearn(self):
+        pass
